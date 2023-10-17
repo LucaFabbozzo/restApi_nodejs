7c35ec35905427f9ef174e90bb52a95eb5e17bd1
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-10-17 14:52:36 +0200

    start to add list of queries into module

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,17 +1,18 @@
 //il mio modulo controller dove scrivero tutta la mia logica di business
 
 //importo il mio modulo con la connessione al mio db
-const pool = require('../../db')
+const pool = require('../../db');
+const queries = require('./queries');
 
 //la funzione con il quale scrivo la logica e la mia query per ricevere tutti gli studenti
 const getStudents = (req, res) => {
-    pool.query("SELECT * FROM students", (error, results) => {
+    pool.query(queries.getStudents, (error, results) => {
         if(error) throw error;
         res.status(200).json(results.rows);
     });
 }
 
-//exporto la mia funzione
+//exporto la mia funzione all'interno di un'oggetto
 module.exports = {
     getStudents,
 };
---
+++ src/student/queries.js
@@ -0,0 +1,9 @@
+//lista di tutte le query inizializzate in variabili, per separarle dalla logica di business
+
+const getStudents = "SELECT * FROM students";
+
+
+
+module.exports = {
+    getStudents,
+};
